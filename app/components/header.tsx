dc420843c0d4db602aa155ef06c4a51cb07157c8
Mahad-Usman-15 <mahadusman2008@gmail.com>
2024-10-16 00:30:56 +0500

    Add files via upload
    This is an blogging website

---
+++ app/components/header.tsx
@@ -0,0 +1,20 @@
+export default function Header(){
+  return (
+  
+    <header className="Header">
+      <div className="Header-name">
+        <h1>BLOGGING IS FUN</h1>
+      </div>
+
+
+    <div className="Header-items">
+        <li className="header-icons">Home</li>
+        <li className="header-icons">Watch</li>
+        <li className="header-icons">Design</li>
+        <li className="header-icons">About</li>
+      </div>
+     
+   
+    </header>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Body from "./components/body";
+export default function Blog(){
+return (
+  <div>
+    
+    <Header/>
+    <Body/>
+      <Footer/>
+    
+  </div>
+)
+}
